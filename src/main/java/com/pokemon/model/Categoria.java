2aba1833d7a6ad4a1bb50752b613bd486c8ee034
MaateusMDS <maateusmania@gmail.com>
2023-05-16 12:20:07 -0300

    adding insert

---
+++ src/main/java/com/pokemon/model/Categoria.java
@@ -0,0 +1,30 @@
+package com.pokemon.model;
+
+public enum Categoria {
+
+    SEED,
+    LIZARD,
+    FLAME,
+    TURTLE,
+    WORM,
+    COCOON,
+    BIRD,
+    MOUSE,
+    SNAKE,
+    POISON,
+    FAIRY,
+    FOX,
+    BAT,
+    WEED,
+    FLOWER,
+    MUSHROOM,
+    INSECT,
+    CAT,
+    DUCK,
+    MONKEY,
+    PSI,
+    SUPERPOWER,
+    ROCK,
+    MAGNET;
+
+}

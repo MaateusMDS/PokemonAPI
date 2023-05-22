4bbb39a5ff33dabaa2b26e860310539b9dcfa98a
MaateusMDS <maateusmania@gmail.com>
2023-05-22 08:30:34 -0300

    bean validation fixed

--- src/main/java/com/pokemon/model/dados/AtualizarPokemon.java
+++ src/main/java/com/pokemon/model/dados/AtualizarPokemon.java
@@ -1,10 +1,15 @@
 package com.pokemon.model.dados;
 
 import com.pokemon.model.Categoria;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
 
 public record AtualizarPokemon(
         String nome,
+        @DecimalMin("0.4") @DecimalMax(value = "10.0", inclusive = false)
         double altura,
+        @Digits(integer=3, fraction=1)
         double peso,
         Categoria categoria,
         String habilidade
--- src/main/java/com/pokemon/model/dados/InserirPokemon.java
+++ src/main/java/com/pokemon/model/dados/InserirPokemon.java
@@ -1,11 +1,16 @@
 package com.pokemon.model.dados;
 
 import com.pokemon.model.Categoria;
+import jakarta.validation.constraints.*;
 
 public record InserirPokemon(
+        @NotBlank
         String nome,
+        @NotNull @DecimalMin("0.4") @DecimalMax(value = "10.0", inclusive = false)
         double altura,
+        @NotNull @Digits(integer=3, fraction=1)
         double peso,
+        @NotNull
         Categoria categoria,
         String habilidade
 ) {
